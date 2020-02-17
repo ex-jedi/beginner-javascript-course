8ea8bea8791d1327f88ba9ad95cb72ca12ee1b34
Mark Phoenix <mark@pixelsmiths.co.uk>
2020-02-17 15:15:15 +0000

    Types numbers. ep8

--- types.js
+++ types.js
@@ -1,19 +1,20 @@
-/* eslint-disable */
-console.log('Hello!');
-const name = "Mark";
+// ********** Strings **********
+const age = 46;
+const name = 'Mark';
+const test = 'Wut';
 const middle = 'Robert';
 const last = `Phoenix`;
 const sentence = `she's so cool`;
 const song = `hey
 
-there
+// there
 
-girl`;
+// girl`;
 
 // const hello = 'my name is ' + name + '. Pleased to meet you';
 
 // looking at backtick
-const hello = `Hello, my name is ${name} ${last}. Pleased to meet you! I am ${40 + 6} years old`;
+const hello = `Hello, my name is ${name} ${last}. Pleased to meet you! I am ${age} years old`;
 const html = `
 <div>
   <h2>${name}</h2>
@@ -22,3 +23,16 @@ const html = `
 `;
 
 document.body.innerHTML = html;
+
+// ********** Numbers **********
+const a = 10;
+const b = 20;
+
+const smarties = 20;
+const kids = 3;
+const dadGets = smarties % kids;
+
+const eachKidGets = Math.floor(smarties / kids);
+
+console.log(`Each kid gets ${eachKidGets}`);
+console.log(`Dad gets ${dadGets}`);
