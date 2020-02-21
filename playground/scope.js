2c76f9e37e304ea8270e2c4687fc017611d73a80
Mark Phoenix <mark@pixelsmiths.co.uk>
2020-02-21 08:49:15 +0000

    Scope. EP17

--- playground/scope.js
+++ playground/scope.js
@@ -1,28 +1,49 @@
+// // Global Variables
+
+// const first = 'Mark';   // Not attached to window
+// let middle = 'Robert'; // Not attached to window
+// var last = 'Phoenix';  // Attached to window
+
+// // Also attached to window
+// function sayHi() {
+//   console.log('Hi.')
+// }
+
 // const age = 100;
 
 // function go() {
-//   const myAge = 200;
-//   const hair = 'blonde';
-//   console.log(age);
-//   console.log(myAge);
-//   console.log(hair);
+//   // A shadow variable
+//   const age = 200;
+//   console.log(age)
+//   const hair = 'Blond';
+//   return hair;
 // }
 
 // go();
-/* eslint-disable*/
+// // let hairCol = go();
+
+// console.log(hair)
+
+// let cool;
+// if (1 === 1) {
+//    cool = true;
+// }
+
+// console.log(cool);
 
 // function isCool(name) {
-//   if (name === 'wes') {
-//     var cool = true;
-//   }
-//   console.log(cool);
-//   return cool;
+//    let cool;
+//    if (name === 'Mark') {
+//       cool = true;
+//    }
+//    console.log(cool);
+//    return cool;
 // }
 
-// for(let i = 0; i < 10; i++) {
-//   console.log(i);
+// // let is block scoped. var is not!
+// for (let i = 0; i < 5; i++) {
+//    console.log(i)
 // }
-
 
 const dog = 'snickers';
 
@@ -32,19 +53,12 @@ function logDog(dog) {
 
 function go() {
   const dog = 'sunny';
-  logDog('sunny');
-}
-
-go();
-
-function yell() {
-  console.log(name.toUpperCase())
+  logDog(dog);
 }
 
 function sayHi(name) {
-
+  function yell() {
+    console.log(name.toUpperCase());
+  }
   yell();
 }
-
-
-yell();
