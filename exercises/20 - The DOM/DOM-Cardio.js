3a97bf3c5dc56aaf3abe0a5b2863a92646bbd9c8
Mark Phoenix <mark@pixelsmiths.co.uk>
2020-03-01 16:59:18 +0000

    Dom cardio first few exercises

--- exercises/20 - The DOM/DOM-Cardio.js
+++ exercises/20 - The DOM/DOM-Cardio.js
@@ -1,22 +1,51 @@
 // Make a div
+const div = document.createElement('div');
 
 // add a class of wrapper to it
+div.classList.add('wrapper');
 
 // put it into the body
+document.body.appendChild(div);
 
 // make an unordered list
+const list = document.createElement('ul');
 
 // add three list items with the words "one, two three" in them
+const liOne = document.createElement('li');
+liOne.textContent = 'One';
+const liTwo = document.createElement('li');
+liTwo.textContent = 'Two';
+const liThree = document.createElement('li');
+liThree.textContent = 'Three';
+
+list.appendChild(liOne);
+
+// liOne.insertAdjacentElement('afterend', liTwo);
+
+list.appendChild(liTwo);
+list.appendChild(liThree);
 // put that list into the above wrapper
+div.appendChild(list);
 
 // create an image
+const image = document.createElement('img');
 
 // set the source to an image
-// set the width to 250
-// add a class of cute
-// add an alt of Cute Puppy
-// Append that image to the wrapper
+image.src = 'https://picsum.photos/200';
 
+// set the width to 250
+image.width = 250;
+
+// add a class of cute
+image.classList.add('cute');
+
+// add an alt of Cute Puppy
+image.alt = 'Cute Puppy';
+
+// Append that image to the wrapper
+div.appendChild(image);
+
+console.log(image);
 // with HTML string, make a div, with two paragraphs inside of it
 // put this div before the unordered list from above
 
