48efd365a083088c3b7fb3253d58dc4159eabc6f
Mark Phoenix <mark@pixelsmiths.co.uk>
2020-03-04 21:03:12 +0000

    Prevent default and key events. EP 31.

--- exercises/29 - Events/forms.js
+++ exercises/29 - Events/forms.js
@@ -8,3 +8,28 @@ wes.addEventListener('click', function(event) {
     event.preventDefault();
   }
 });
+
+const signupForm = document.querySelector('[name="signup"]');
+
+signupForm.addEventListener('submit', function(event) {
+  event.preventDefault();
+  const name = event.currentTarget.name.value;
+  if (name.includes('chad')) {
+    alert('Sorry, no Chads');
+  }
+});
+
+function logEvent(event) {
+  console.log(event.type);
+  console.log(event.currentTarget.value);
+}
+
+signupForm.name.addEventListener('keyup', logEvent);
+signupForm.name.addEventListener('keydown', logEvent);
+signupForm.name.addEventListener('focus', logEvent);
+signupForm.name.addEventListener('blur', logEvent);
+
+// 'keyup'
+// 'keydown'
+// 'focus'
+// 'blur'
