505d159af19fa9ced4f617d2de93749339b1f412
Mark Phoenix <mark@pixelsmiths.co.uk>
2020-03-10 08:39:49 +0000

    Finished etch-a-sketch. EP33.

--- exercises/33 - Etch-a-Sketch/etch-a-sketch.js
+++ exercises/33 - Etch-a-Sketch/etch-a-sketch.js
@@ -2,7 +2,7 @@
 const canvas = document.querySelector('#etch-a-sketch');
 const ctx = canvas.getContext('2d');
 const shakebutton = document.querySelector('.shake');
-const MOVE_AMOUNT = 10;
+const MOVE_AMOUNT = 20;
 
 // Setup canvas for drawing
 // Make variables called width and height from those same properties on the canvas object
@@ -16,6 +16,8 @@ ctx.lineJoin = 'round';
 ctx.lineCap = 'round';
 ctx.lineWidth = MOVE_AMOUNT;
 
+let hue = Math.random() * 360;
+ctx.strokeStyle = `hsl(${hue}, 100%, 50%)`;
 ctx.beginPath();
 ctx.moveTo(x, y);
 ctx.lineTo(x, y);
@@ -23,6 +25,9 @@ ctx.stroke();
 
 // Write draw function
 function draw({ key }) {
+  // Increment hue
+  hue += 10;
+  ctx.strokeStyle = `hsl(${hue}, 100%, 50%)`;
   console.log(key);
   ctx.beginPath();
   ctx.moveTo(x, y);
@@ -57,6 +62,20 @@ function handleKey(e) {
 }
 
 // Clear / Shake function
+function clearCanvas() {
+  canvas.classList.add('shake');
+  ctx.clearRect(0, 0, width, height);
+  canvas.addEventListener(
+    'animationend',
+    function() {
+      canvas.classList.remove('shake');
+      console.log('Done the Shake');
+    },
+    { once: true }
+  );
+}
 
 // Listen for arrow keys
 window.addEventListener('keydown', handleKey);
+// Listen for skake / clear canvas button
+shakebutton.addEventListener('click', clearCanvas);
