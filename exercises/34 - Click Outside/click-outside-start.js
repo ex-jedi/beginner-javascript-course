c3c0e8a60a8c02b1ca41a8193f4d25cdbf056aa8
Mark Phoenix <mark@pixelsmiths.co.uk>
2020-03-17 20:50:24 +0000

    Working modal reveal

--- exercises/34 - Click Outside/click-outside-start.js
+++ exercises/34 - Click Outside/click-outside-start.js
@@ -1,9 +1,24 @@
 const cardButtons = document.querySelectorAll('.card-button');
+const modalOuter = document.querySelector('.modal-outer');
+const modalInner = document.querySelector('.modal-inner');
 
-function handleCardButtonClick() {
-  console.log('You clicked it!');
+function handleCardButtonClick(event) {
+  const button = event.currentTarget;
+  const card = button.closest('.card');
+  // Get image src
+  const imgSrc = card.querySelector('img').src;
+  // Get card description data attribute
+  const desc = card.dataset.description;
+  const name = card.querySelector('h2').textContent;
+  // Populate modal with card info
+  modalInner.innerHTML = `
+    <img src="${imgSrc.replace('200', '600')}" alt="${name}"/>
+    <p>${desc}</p>
+  `;
+  modalOuter.classList.add('open');
 }
 
 cardButtons.forEach(button =>
   button.addEventListener('click', handleCardButtonClick)
 );
+// Show the modal
