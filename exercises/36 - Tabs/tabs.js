c813f5e0f73dc5862891a58657e118d264511176
Mark Phoenix <mark@pixelsmiths.co.uk>
2020-03-27 16:56:38 +0000

    Completed with three methods

--- exercises/36 - Tabs/tabs.js
+++ exercises/36 - Tabs/tabs.js
@@ -1,6 +1,6 @@
 const tabs = document.querySelector('.tabs');
 const tabButtons = tabs.querySelectorAll('[role="tab"]');
-const tabPanels = document.querySelectorAll('[role="tabpanel"]');
+const tabPanels = Array.from(document.querySelectorAll('[role="tabpanel"]'));
 
 function handleTabClick(event) {
   // Hide non clicked tab panels
@@ -14,9 +14,28 @@ function handleTabClick(event) {
   // Mark the clicked tab as selected
   event.currentTarget.setAttribute('aria-selected', true);
   // Find the associated tab panel and show it
+
   const { id } = event.currentTarget;
+  /*
+ Method one
   console.log(id);
   const tabPanel = tabs.querySelector(`[aria-labelledby="${id}"]`);
+  tabPanel.hidden = false;
+  */
+
+  /*
+  Method two(my version) - find in the array of tabPanel
+  tabPanels.find(panel => {
+    if (panel.getAttribute('aria-labelledby') === id) {
+      panel.hidden = false;
+    }
+  });
+  */
+
+  // Method two(wes version) - find in the array of tabPanel
+  const tabPanel = tabPanels.find(
+    panel => panel.getAttribute('aria-labelledby') === id
+  );
   tabPanel.hidden = false;
 }
 
