95354119979b24076a63142d17af5e71343a6b52
Mark Phoenix <mark@pixelsmiths.co.uk>
2020-04-20 13:49:21 +0100

    Face detection active

--- exercises/55 - Face Detection Censorship/pixelated-face-start.js
+++ exercises/55 - Face Detection Censorship/pixelated-face-start.js
@@ -1,11 +1,8 @@
 const video = document.querySelector('.webcam');
-
 const canvas = document.querySelector('.video');
 const ctx = canvas.getContext('2d');
-
 const faceCanvas = document.querySelector('.face');
 const faceCtx = canvas.getContext('2d');
-
 const faceDetector = new window.FaceDetector();
 
 // Populate video function
@@ -15,7 +12,19 @@ async function populateVideo() {
     video: { width: 1280, height: 720 },
   });
   video.srcObject = stream;
-  video.play();
+  await video.play();
+  // Make canvases same size as video
+  canvas.width = video.videoWidth;
+  canvas.height = video.videoHeight;
+  faceCanvas.width = video.videoWidth;
+  faceCanvas.height = video.videoHeight;
 }
 
-populateVideo();
+async function detect() {
+  const faces = await faceDetector.detect(video);
+  console.log(faces.length);
+  // Ask browser when next animation frame is and get it to run detect
+  requestAnimationFrame(detect);
+}
+
+populateVideo().then(detect);
