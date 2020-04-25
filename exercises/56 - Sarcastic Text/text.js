768422dbd3f6ec98869dbb9d1ec883adc59f01eb
Mark Phoenix <mark@pixelsmiths.co.uk>
2020-04-25 18:41:17 +0100

    Starting lesson

---
+++ exercises/56 - Sarcastic Text/text.js
@@ -0,0 +1,4 @@
+const textarea = document.querySelector('[name="text"]');
+const result = document.querySelector('.result');
+const filterInputs = document.querySelectorAll('[name="filter"]');
+console.log(textarea, result, filterInputs);
