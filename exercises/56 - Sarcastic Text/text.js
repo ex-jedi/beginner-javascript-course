0eed1142552d528cca55da4c270262d155bbad81
Mark Phoenix <mark@pixelsmiths.co.uk>
2020-04-26 16:51:56 +0100

    Splitting textbox input into array

--- exercises/56 - Sarcastic Text/text.js
+++ exercises/56 - Sarcastic Text/text.js
@@ -1,4 +1,18 @@
 const textarea = document.querySelector('[name="text"]');
 const result = document.querySelector('.result');
 const filterInputs = document.querySelectorAll('[name="filter"]');
-console.log(textarea, result, filterInputs);
+
+function transformText(text) {
+  // Loop over each letter of text
+  const mod = Array.from(text);
+  console.log(mod);
+  result.textContent = text;
+}
+
+const filters = {
+  sarcastic() {},
+  funky() {},
+  unable() {},
+};
+
+textarea.addEventListener('input', e => transformText(e.target.value));
