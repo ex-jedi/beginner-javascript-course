76fd57f1b45178da63a90d1b46f4fee0127d5f06
Mark Phoenix <mark@pixelsmiths.co.uk>
2020-07-15 08:32:44 +0100

    First few selectors

--- exercises/58 - Gallery/gallery-prototype.js
+++ exercises/58 - Gallery/gallery-prototype.js
@@ -1,3 +1,20 @@
 // https://courses.wesbos.com/account/access/5e4818abd9cc836465201439/view/375805123
 
-function
+function Gallery(gallery) {
+  if (!gallery) {
+    throw new Error('No Gallery Found');
+  }
+  // Selecting images in this function to maintain scope so this can be reused
+  // Selecting within gallery & not document as there are multiple galleries on the page
+  const images = Array.from(gallery.querySelectorAll('img'));
+  // There will only be one modal on the page so selecting using document
+  const modal = document.querySelector('.modal');
+  // Selecting buttons in modal
+  const prevButton = modal.querySelector('.prev');
+  const nextButton = modal.querySelector('.next');
+}
+
+// Use it on the page
+
+const gallery1 = Gallery(document.querySelector('.gallery1'));
+const gallery2 = Gallery(document.querySelector('.gallery2'));
