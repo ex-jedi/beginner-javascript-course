f458b1585a089c1eaa28b8dc2237370a3cf03978
Mark Phoenix <mark@pixelsmiths.co.uk>
2020-07-17 12:38:37 +0100

    Changing img totle and description

--- exercises/58 - Gallery/gallery-prototype.js
+++ exercises/58 - Gallery/gallery-prototype.js
@@ -12,9 +12,11 @@ function Gallery(gallery) {
   // There will only be one modal on the page so selecting using document
   const modal = document.querySelector('.modal');
 
-  // Selecting buttons in modal
+  // Selecting previous and next buttons in modal
   const prevButton = modal.querySelector('.prev');
   const nextButton = modal.querySelector('.next');
+  // Keep track of current image open
+  let currentImage;
 
   // Show images function
   function showImage(el) {
@@ -23,7 +25,14 @@ function Gallery(gallery) {
       console.info('No image to show');
     }
     console.log(el);
+    // Change modal image source when image is clicked
     modal.querySelector('img').src = el.src;
+    // Change modal title when image is clicked
+    modal.querySelector('h2').textContent = el.title;
+    // Change modal description paragraph text when image is clicked. Uses data attribute
+    modal.querySelector('figure p').textContent = el.dataset.description;
+    // Update current image (stores what image is open)
+    currentImage = el;
   }
 
   // Loop over images and add event listener. Callback function passes current target as argument to show image function
