555d055f94dde227ae61c409a28fa1cad568e2c0
Mark Phoenix <mark@pixelsmiths.co.uk>
2020-07-17 12:18:49 +0100

    Click event listener on images

--- exercises/58 - Gallery/gallery-prototype.js
+++ exercises/58 - Gallery/gallery-prototype.js
@@ -4,14 +4,32 @@ function Gallery(gallery) {
   if (!gallery) {
     throw new Error('No Gallery Found');
   }
+
   // Selecting images in this function to maintain scope so this can be reused
   // Selecting within gallery & not document as there are multiple galleries on the page
   const images = Array.from(gallery.querySelectorAll('img'));
+
   // There will only be one modal on the page so selecting using document
   const modal = document.querySelector('.modal');
+
   // Selecting buttons in modal
   const prevButton = modal.querySelector('.prev');
   const nextButton = modal.querySelector('.next');
+
+  // Show images function
+  function showImage(el) {
+    // Safety thing to prevent breaking if there's no image
+    if (!el) {
+      console.info('No image to show');
+    }
+    console.log(el);
+  }
+
+  function handleImageClick(event) {
+    showImage(event.currentTarget);
+  }
+
+  images.forEach(image => image.addEventListener('click', handleImageClick));
 }
 
 // Use it on the page
