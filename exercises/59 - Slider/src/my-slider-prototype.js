097fbe97091e26e38aca35125b821be7048b26a6
Mark Phoenix <mark@pixelsmiths.co.uk>
2020-08-09 16:49:07 +0100

    Refactored

--- exercises/59 - Slider/src/my-slider-prototype.js
+++ exercises/59 - Slider/src/my-slider-prototype.js
@@ -2,89 +2,87 @@ function Slider(slider) {
   if (!(slider instanceof Element)) {
     throw new Error('No slider passed in');
   }
-  // Make variables for working with slider
-  // These are needed in a number of places so we're making them here
-  let prev;
-  let current;
-  let next;
+
   // Select elements needed for slider
-  const slides = slider.querySelector('.slides');
+  this.slider = slider;
+  this.slides = slider.querySelector('.slides');
   const prevButton = slider.querySelector('.goToPrev');
   const nextButton = slider.querySelector('.goToNext');
 
-  function startSlider() {
-    current = slider.querySelector('.current') || slides.firstElementChild;
-    prev = current.previousElementSibling || slides.lastElementChild;
-    next = current.nextElementSibling || slides.firstElementChild;
-  }
-
-  // Adds classes to previous, current and next slides
-  function applyClasses() {
-    prev.classList.add('prev');
-    current.classList.add('current');
-    next.classList.add('next');
-  }
-
-  function move(direction) {
-    // Strip all classes from slides
-    const classesToRemove = ['prev', 'current', 'next'];
-    // Fancy way. Make array ow elements, loop over and spread classes to remove as argument
-    // [prev, current, next].forEach(el => el.classlist.remove(...classesToRemove));
-    // This does the same but is maybe more readable
-    prev.classList.remove(...classesToRemove);
-    current.classList.remove(...classesToRemove);
-    next.classList.remove(...classesToRemove);
-    // Get direction ov movement and update slide classes accordingly
-    // Shifting all slides either one forwards or one backwards
-    if (direction === 'back') {
-      // Make new array of current values, use destructuring to overwrite them
-      [prev, current, next] = [
-        // Get previous slide or the very last slide - enables wrapping
-        prev.previousElementSibling || slides.lastElementChild,
-        prev,
-        current,
-      ];
-    } else {
-      [prev, current, next] = [
-        current,
-        next,
-        // Get next slide or very first slide - enables wrapping
-        next.nextElementSibling || slides.firstElementChild,
-      ];
-    }
-    applyClasses();
-  }
-
   // When this slider is created run the startSlider function
-  startSlider();
-  applyClasses();
+  this.startSlider();
+  this.applyClasses();
 
   // Event listeners
   // Hook up Prev and Next Buttons
-  prevButton.addEventListener('click', () => move('back'));
-  nextButton.addEventListener('click', move); // Can just pass reference to function as we don't teen to pass in direction as we have the else option handling this above
-
-  // Move slider with arrow keys
-
-  // Check arrow key press
-  function arrowKeyHandler(e) {
-    if (e.key === 'ArrowLeft') {
-      move('back');
-    } else if (e.key === 'ArrowRight') {
-      move();
-    }
-  }
+  prevButton.addEventListener('click', () => this.move('back'));
+  nextButton.addEventListener('click', () => this.move()); // Can just pass reference to function as we don't teen to pass in direction as we have the else option handling this above
 
   // Add arrowKeyHandler when slider is focused
+  this.arrowKeyHandler = this.arrowKeyHandler.bind(this);
   slider.addEventListener('focus', () => {
-    window.addEventListener('keyup', arrowKeyHandler);
+    window.addEventListener('keyup', this.arrowKeyHandler);
   });
 
   // Remove arrowKeyHandler when slider looses focus
   slider.addEventListener('blur', () => {
-    window.removeEventListener('keyup', arrowKeyHandler);
+    window.removeEventListener('keyup', this.arrowKeyHandler);
   });
 }
 
-const mySlider = Slider(document.querySelector('.slider'));
-const dogSlider = Slider(document.querySelector('.dog-slider'));
+Slider.prototype.startSlider = function() {
+  this.current = this.slider.querySelector('.current') || this.slides.firstElementChild;
+  this.prev = this.current.previousElementSibling || this.slides.lastElementChild;
+  this.next = this.current.nextElementSibling || this.slides.firstElementChild;
+};
+
+// Adds classes to previous, current and next slides
+Slider.prototype.applyClasses = function() {
+  this.prev.classList.add('prev');
+  this.current.classList.add('current');
+  this.next.classList.add('next');
+};
+
+Slider.prototype.move = function(direction) {
+  // Strip all classes from slides
+  const classesToRemove = ['prev', 'current', 'next'];
+  // Fancy way. Make array ow elements, loop over and spread classes to remove as argument
+  // [prev, current, next].forEach(el => el.classlist.remove(...classesToRemove));
+  // This does the same but is maybe more readable
+  this.prev.classList.remove(...classesToRemove);
+  this.current.classList.remove(...classesToRemove);
+  this.next.classList.remove(...classesToRemove);
+  // Get direction ov movement and update slide classes accordingly
+  // Shifting all slides either one forwards or one backwards
+  if (direction === 'back') {
+    // Make new array of current values, use destructuring to overwrite them
+    [this.prev, this.current, this.next] = [
+      // Get previous slide or the very last slide - enables wrapping
+      this.prev.previousElementSibling || this.slides.lastElementChild,
+      this.prev,
+      this.current,
+    ];
+  } else {
+    [this.prev, this.current, this.next] = [
+      this.current,
+      this.next,
+      // Get next slide or very first slide - enables wrapping
+      this.next.nextElementSibling || this.slides.firstElementChild,
+    ];
+  }
+  this.applyClasses();
+};
+
+// Move slider with arrow keys
+Slider.prototype.arrowKeyHandler = function(e) {
+  if (e.key === 'ArrowLeft') {
+    this.move('back');
+  } else if (e.key === 'ArrowRight') {
+    this.move();
+  }
+};
+
+const mySlider = new Slider(document.querySelector('.slider'));
+const dogSlider = new Slider(document.querySelector('.dog-slider'));
+
+console.log(mySlider, dogSlider);
