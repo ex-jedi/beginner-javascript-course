3f270fd11dde49880f8bb931bc9fc5867690237d
Mark Phoenix <mark@pixelsmiths.co.uk>
2020-08-20 16:53:40 +0100

    Interesting random number generating function

--- exercises/73 - Async Typer/scripts.js
+++ exercises/73 - Async Typer/scripts.js
@@ -1,6 +1,7 @@
 // https://courses.wesbos.com/account/access/5e4818abd9cc836465201439/view/375835674
 const wait = (ms = 0) => new Promise(resolve => setTimeout(resolve, ms));
 
-function getRandomBetween(min = 20, max = 150) {
-  return Math.floor(Math.random() * (max - min) + min);
+// Done this way so you can add a number as a third argument to test the function
+function getRandomBetween(min = 20, max = 150, randomNumber = Math.random()) {
+  return Math.floor(randomNumber * (max - min) + min);
 }
