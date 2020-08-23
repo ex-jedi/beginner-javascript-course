69ccf73285a4c9f4bedd78a2dadb349b62a4fb40
Mark Phoenix <mark@pixelsmiths.co.uk>
2020-08-23 12:50:18 +0100

    Notes

---
+++ exercises/75 - CORS and Recipes/cors-recipies-mine.js
@@ -0,0 +1,10 @@
+// https://courses.wesbos.com/account/access/5e4818abd9cc836465201439/view/375840774
+// CORS - Cross Origin Resource Sharing. By default you're prevented from sharing data from one domain to another. CORS policy is on the server. Sometimes sites don't implement one so you might have to work around. In this instance a CORS proxy, some other server that'll be between your site and the target site. Don't use for sensitive information!!
+const baseEndpoint = 'http://www.recipepuppy.com/api';
+
+async function fetchRecipes(query) {
+  const res = await fetch(`${baseEndpoint}?q=${query}`);
+  const data = await res.json();
+}
+
+fetchRecipes('pizza');
