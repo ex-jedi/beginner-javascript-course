4400cc329a1fcabc94e016f69384db4a0569eb0c
Mark Phoenix <mark@pixelsmiths.co.uk>
2020-08-25 18:09:13 +0100

    Completed

--- exercises/77 - Currency Converter/currenct-converter-mine.js
+++ exercises/77 - Currency Converter/currenct-converter-mine.js
@@ -1,9 +1,14 @@
 // https://courses.wesbos.com/account/access/5e4818abd9cc836465201439/view/375929658
 // Capturing select inputs with attribute selectors
+const fromInput = document.querySelector('[name="from_amount"]');
 const fromSelect = document.querySelector('[name="from_currency"]');
 const toSelect = document.querySelector('[name="to_currency"]');
+const toAmount = document.querySelector('.to_amount');
 const endPoint = 'https://api.exchangeratesapi.io/latest';
 const ratesByBase = {}; // Where we're going to store the rates
+const form = document.querySelector('.app form');
+const locale = navigator.language;
+console.log(locale);
 
 const currencies = {
   USD: 'United States Dollar',
@@ -76,11 +81,26 @@ async function convert(amount, from, to) {
 
   const rate = ratesByBase[from].rates[to];
   const convertedAmount = rate * amount;
-  console.log(`${amount} ${from} is ${convertedAmount} in ${to}`);
+  // console.log(`${amount} ${from} is ${convertedAmount} in ${to}`);
   return convertedAmount;
+}
+
+function formatCurrency(amount, currency) {
+  return Intl.NumberFormat(locale, {
+    style: 'currency',
+    currency,
+  }).format(amount);
+}
+
+async function handleInput(e) {
+  const rawAmount = await convert(fromInput.value, fromSelect.value, toSelect.value);
+  console.log(rawAmount);
+  toAmount.textContent = formatCurrency(rawAmount, toSelect.value);
 }
 
 // Storing generateOptions output in variable as it'll be used twice. Better than running it twice
 const optionsHtml = generateOptions(currencies);
 fromSelect.innerHTML = optionsHtml;
 toSelect.innerHTML = optionsHtml;
+
+form.addEventListener('input', handleInput);
