e69cd3456e3b1e9f0a4ab7dbe06522730e50b720
Mark Phoenix <mark@pixelsmiths.co.uk>
2020-08-25 16:34:18 +0100

    Convert working

--- exercises/77 - Currency Converter/currenct-converter-mine.js
+++ exercises/77 - Currency Converter/currenct-converter-mine.js
@@ -3,6 +3,7 @@
 const fromSelect = document.querySelector('[name="from_currency"]');
 const toSelect = document.querySelector('[name="to_currency"]');
 const endPoint = 'https://api.exchangeratesapi.io/latest';
+const ratesByBase = {}; // Where we're going to store the rates
 
 const currencies = {
   USD: 'United States Dollar',
@@ -56,8 +57,26 @@ function generateOptions(options) {
 async function fetchRates(base = 'USD') {
   const res = await fetch(`${endPoint}?base=${base}`);
   const rates = await res.json();
-  console.log(rates);
+  // console.log(rates);
   return rates;
+}
+
+// We don't want to fetch the rates from the api each time as that'll be very slow. So we need to cache the rates
+async function convert(amount, from, to) {
+  // Check if we have the rates for the chosen currency
+  if (!ratesByBase[from]) {
+    console.log(`Ruh roh, we don't have ${from} rate to convert your ${amount} ${from} to ${to}. So let's get it.`);
+    // If we don't, get them
+    const rates = await fetchRates(from);
+    console.log(rates);
+    // Then store them in our object
+    ratesByBase[from] = rates;
+    console.log(ratesByBase);
+  }
+
+  const rate = ratesByBase[from].rates[to];
+  const convertedAmount = rate * amount;
+  console.log(`${amount} ${from} is ${convertedAmount} in ${to}`);
 }
 
 // Storing generateOptions output in variable as it'll be used twice. Better than running it twice
