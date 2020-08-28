8afc41cc7f3a7493de142916ecb7f1055260e872
Mark Phoenix <mark@pixelsmiths.co.uk>
2020-08-28 13:40:11 +0100

    Async loading import

--- playground/modules/handlers-mine.js
+++ playground/modules/handlers-mine.js
@@ -1,3 +1,7 @@
-export function handleButtonClick(e) {
-  console.log(e);
+// Loading on demand
+// import { currencies } from './currencies-mine.js';
+
+export async function handleButtonClick(e) {
+  const currenciesModule = await import('./currencies-mine.js');
+  console.log(currenciesModule.default);
 }
