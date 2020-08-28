3e7c16d3219850ff95ba080a62f790a72de3bde9
Mark Phoenix <mark@pixelsmiths.co.uk>
2020-08-28 12:11:47 +0100

    Named imports or exports

--- playground/modules/scripts-mine.js
+++ playground/modules/scripts-mine.js
@@ -1,7 +1,9 @@
 // https://courses.wesbos.com/account/access/5e4818abd9cc836465201439/view/375926026
 
-import { returnHi } from './utils-mine';
+import { returnHi, middle, last } from './utils-mine';
 
 const name = 'Mark';
 
 console.log(returnHi(name));
+console.log(middle);
+console.log(last);
--- playground/modules/utils-mine.js
+++ playground/modules/utils-mine.js
@@ -1,5 +1,14 @@
-const last = 'Phoenix';
+// Variables are scoped to each file/module. They will be available to function in the same module
+// const last = 'Phoenix';
+
+// To export a variable add export in front of it
+const middle = 'Robert';
+export const last = 'Phoenix';
 
 export function returnHi(name) {
   return `Hi ${name} ${last}`;
 }
+
+// Can also use named exports. Really a matter of preference. WEs uses both
+
+export { middle };
