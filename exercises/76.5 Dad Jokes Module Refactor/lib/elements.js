6479e4f1163b78be51efacb891a7ec66aa54c1b9
Mark Phoenix <mark@pixelsmiths.co.uk>
2020-09-01 20:04:15 +0100

    Stuff

--- exercises/76.5 Dad Jokes Module Refactor/lib/elements.js
+++
@@ -1,4 +0,0 @@
-export const jokeButton = document.querySelector('.getJoke');
-export const jokeHolder = document.querySelector('.joke p');
-export const loader = document.querySelector('.loader');
-export const jokeText = document.querySelector('.jokeText');
