4404d7515dca4095284264ad5fb3e2148f45006a
Mark Phoenix <mark@pixelsmiths.co.uk>
2020-09-03 18:00:30 +0100

    Seting up recognition

---
+++ exercises/84 - Web Speech Colour Game Mine/handlers.js
@@ -0,0 +1,4 @@
+export function handleResult(e) {
+  console.log('working');
+  console.log(e);
+}
--- exercises/84 - Web Speech Colour Game Mine/speech-dist.js
+++ exercises/84 - Web Speech Colour Game Mine/speech-dist.js
@@ -1,3 +1,5 @@
+import { handleResult } from './handlers';
+
 window.SpeechRecognition = window.SpeechRecognition || window.webkitSpeechRecognition;
 
 function start() {
@@ -11,6 +13,9 @@ function start() {
     recognition.continuous = true;
     // Will give results while speaking instead of waiting to the end
     recognition.interimResults = true;
+    // Listen for speech (doesn't ley you add a typical event listener)
+    recognition.onresult = handleResult;
+    recognition.start();
     console.log(recognition);
   }
 }
