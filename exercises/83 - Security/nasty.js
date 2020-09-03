1e343b58710caf099836b05075c9dc3d37ea44fb
Mark Phoenix <mark@pixelsmiths.co.uk>
2020-09-03 13:30:35 +0100

    Unsanitized html input field

--- exercises/83 - Security/nasty.js
+++ exercises/83 - Security/nasty.js
@@ -1,14 +1,19 @@
-import { sanitize } from 'dompurify';
+// https://courses.wesbos.com/account/access/5e4818abd9cc836465201439/view/375777421
+// Remember that all your code can be read!
+// Take care when putting api keys
+// Careful to make sure that vital data, such as prices can't be changed client side
+// When you allow users to in
+// import { sanitize } from 'dompurify.js';
 
 const input = document.querySelector('[name="input"]');
 const output = document.querySelector('.output');
 const buttons = document.querySelectorAll('nav button');
 input.addEventListener('input', () => {
-  const clean = sanitize(input.value, {
-    FORBID_ATTR: ['width', 'height', 'style'],
-    FORBID_TAGS: ['style'],
-  });
-  output.innerHTML = clean.replace(/\n/g, '<br>');
+  // const clean = sanitize(input.value, {
+  //   FORBID_ATTR: ['width', 'height', 'style'],
+  //   FORBID_TAGS: ['style'],
+  // });
+  output.innerHTML = input.value.replace(/\n/g, '<br>');
 });
 
 // trigger an input even on page load
@@ -19,3 +24,5 @@ buttons.forEach(button =>
     alert(e.currentTarget.textContent);
   })
 );
+
+console.log('Hi');
