df8bc25f5047584db83ac1951269e18c8ea41e9c
Mark Phoenix <mark@pixelsmiths.co.uk>
2020-09-07 08:59:53 +0100

    Marking buttons as not selected on click

--- exercises/36 - Tabs/tabs.js
+++ exercises/36 - Tabs/tabs.js
@@ -4,7 +4,13 @@ const tabButtons = tabs.querySelectorAll('[role="tab"]');
 const tabPanels = tabs.querySelectorAll('[role="tabpanel"]');
 
 function handleTabClick(e) {
-  console.log(e);
+  // Hide all tab panels. Easier to hide them all then show the one you want.
+  tabPanels.forEach(panel => (panel.hidden = true));
+  // Mark all tabs as unselected
+  // Have to use button.setAttribute('aria-selected', false) rather than button.ariaSelected = false for cross browser reasons
+  tabButtons.forEach(button => button.setAttribute('aria-selected', false));
+  // Mark clicked tab button as selected
+  // Find associated tab panel and show it
 }
 
 tabButtons.forEach(button => button.addEventListener('click', handleTabClick));
