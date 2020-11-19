1510fe566192ec936e151115e7aed3cb6833f0ab
Mark <mark@pixelsmiths.co.uk>
2020-11-19 14:03:07 +0000

    On demand exports

--- exercises/Modules/handlers.js
+++ exercises/Modules/handlers.js
@@ -1,5 +1,14 @@
-function handleButtonClick(e) {
-  console.log(e);
+// On demand import
+// async function handleButtonClick() {
+//   const currenciesModule = await import('./currencies.js');
+//   console.log(currenciesModule);
+// }
+
+// Same with some destructuring
+async function handleButtonClick() {
+  const { localCurrency, default: currencies } = await import('./currencies.js');
+  console.log('localCurrency', localCurrency);
+  console.log('currencies', currencies);
 }
 
 export { handleButtonClick };
