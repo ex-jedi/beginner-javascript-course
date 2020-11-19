6485eefcf81612d13f9dd1a0ca9c1bb0dc9bc3f5
Mark <mark@pixelsmiths.co.uk>
2020-11-19 13:32:48 +0000

    Various types of imports

--- exercises/Modules/mark.js
+++ exercises/Modules/mark.js
@@ -5,3 +5,10 @@ const person = {
 
 // Default export
 export default person;
+
+export const dog = 'Rusty';
+export const food = 'chicken';
+
+export function eat(food = 'dog food') {
+  console.log(`${dog} is eating ${food}`);
+}
--- exercises/Modules/scripts.js
+++ exercises/Modules/scripts.js
@@ -1,7 +1,8 @@
+// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Guide/Modules
 // Default import. Only one per module can be exported from each module. Tends to be used if the module does one thing.
 // import person from './mark.js';
-// DEfaults can be names whatever you want
-import thingy from './mark.js';
+// Defaults can be names whatever you want
+// import thingy from './mark.js';
 
 // Named imports. You have to know what name they've been exported as. Can export multiple.
 // import { returnHi, last } from './utils.js';
@@ -9,6 +10,11 @@ import thingy from './mark.js';
 // Can mix named and default imports from one file
 // Can rename imports
 import first, { returnHi as sayHi, last, returnHi } from './utils.js';
+
+// Import everything from a module. Exports an object type thing
+import * as everything from './mark.js';
+
+console.log(everything);
 
 const name = 'Mark';
 
@@ -21,4 +27,4 @@ console.log(first);
 console.log(last);
 
 // console.log(person);
-console.log(thingy);
+// console.log(thingy);
