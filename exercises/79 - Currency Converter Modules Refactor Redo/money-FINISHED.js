cf895451523db13d60b9416193cb5a2c089488d9
Mark <mark@pixelsmiths.co.uk>
2020-11-21 17:01:41 +0000

    Working, but...

--- exercises/79 - Currency Converter Modules Refactor Redo/money-FINISHED.js
+++ exercises/79 - Currency Converter Modules Refactor Redo/money-FINISHED.js
@@ -3,18 +3,10 @@
 // *=========================================
 
 // Default import doesn't need curly brackets
-import currencies from './currencies';
 
 // Named imports use curly brackets
 import { generateOptions } from './utils';
-import { handleInput } from './handlers';
-import { fromSelect, toSelect } from './elements';
 
-const form = document.querySelector('.app form');
+import { init } from './lib';
 
-const optionsHTML = generateOptions(currencies);
-// populate the options elements
-fromSelect.innerHTML = optionsHTML;
-toSelect.innerHTML = optionsHTML;
-
-form.addEventListener('input', handleInput);
+init();
