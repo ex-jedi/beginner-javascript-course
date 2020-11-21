35c0a571db5906f8f219b0fe42036bdc11a0694c
Mark <mark@pixelsmiths.co.uk>
2020-11-21 17:08:20 +0000

    Moving stuff to init file & start on mouseover

--- exercises/79 - Currency Converter Modules Refactor Redo/money-FINISHED.js
+++ exercises/79 - Currency Converter Modules Refactor Redo/money-FINISHED.js
@@ -4,4 +4,6 @@
 
 import { init } from './init';
 
-init();
+// Only start app when someone mouses over it
+const app = document.querySelector('.app');
+app.addEventListener('mouseenter', init, { once: true });
