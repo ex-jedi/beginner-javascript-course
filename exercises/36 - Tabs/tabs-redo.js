9640429e0be1810edda248c1fea6ea6947f486b1
Mark <mark@pixelsmiths.co.uk>
2021-02-04 13:40:30 +0000

    Setting up

--- exercises/36 - Tabs/tabs-redo.js
+++ exercises/36 - Tabs/tabs-redo.js
@@ -1,1 +1,9 @@
-console.log('Hello');
+const tabs = document.querySelector('.tabs');
+const tabButtons = tabs.querySelectorAll('[role="tab"]'); // Looking in tabs rather than document so you can have multiple tabs on a page
+const tabPanels = tabs.querySelectorAll('[role="tab"]'); // Looking in tabs rather than document so you can have multiple tabs on a page
+
+function handleTabClick(e) {
+  console.log(e.currentTarget);
+}
+
+tabButtons.forEach((button) => button.addEventListener('click', handleTabClick));
