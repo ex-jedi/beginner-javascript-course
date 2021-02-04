ae5fc08b92334c3e0a090a96d92f9453ce584c20
Mark <mark@pixelsmiths.co.uk>
2021-02-04 14:15:37 +0000

    Completed method two

--- exercises/36 - Tabs/tabs-redo.js
+++ exercises/36 - Tabs/tabs-redo.js
@@ -1,6 +1,6 @@
 const tabs = document.querySelector('.tabs');
 const tabButtons = tabs.querySelectorAll('[role="tab"]'); // Looking in tabs rather than document so you can have multiple tabs on a page
-const tabPanels = tabs.querySelectorAll('[role="tabpanel"]'); // Looking in tabs rather than document so you can have multiple tabs on a page
+const tabPanels = Array.from(tabs.querySelectorAll('[role="tabpanel"]')); // Looking in tabs rather than document so you can have multiple tabs on a page
 
 function handleTabClick(e) {
   // Hide all tab panels
@@ -11,7 +11,13 @@ function handleTabClick(e) {
   e.currentTarget.setAttribute('aria-selected', true);
   // Find associated tab panel and select it
   const { id } = e.currentTarget;
-  const panelToShow = tabs.querySelector(`[aria-labelledby="${id}"]`);
+
+  // Method one 👇🏾
+  // const panelToShow = tabs.querySelector(`[aria-labelledby="${id}"]`);
+  // panelToShow.hidden = false;
+
+  // Method 2
+  const panelToShow = tabPanels.find((panel) => panel.getAttribute('aria-labelledby') === id);
   panelToShow.hidden = false;
 }
 
