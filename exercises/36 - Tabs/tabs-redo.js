ec0701216cf79d94c5e7635902af224eb90cc9a0
Mark <mark@pixelsmiths.co.uk>
2021-02-04 13:46:30 +0000

    Hiding panels

--- exercises/36 - Tabs/tabs-redo.js
+++ exercises/36 - Tabs/tabs-redo.js
@@ -1,9 +1,13 @@
 const tabs = document.querySelector('.tabs');
 const tabButtons = tabs.querySelectorAll('[role="tab"]'); // Looking in tabs rather than document so you can have multiple tabs on a page
-const tabPanels = tabs.querySelectorAll('[role="tab"]'); // Looking in tabs rather than document so you can have multiple tabs on a page
+const tabPanels = tabs.querySelectorAll('[role="tabpanel"]'); // Looking in tabs rather than document so you can have multiple tabs on a page
 
 function handleTabClick(e) {
-  console.log(e.currentTarget);
+  // Hide all tab panels
+  tabPanels.forEach((panel) => (panel.hidden = true));
+  // Mark all panels as unselected
+  // Mark the clicked tab as selected
+  // Find associated tab panel and select it
 }
 
 tabButtons.forEach((button) => button.addEventListener('click', handleTabClick));
